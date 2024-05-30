17bad5438c8dd67effa511ae91c99e0ee7794e75
Alex Shortt <hello@alex-shortt.com>
2024-05-30 15:16:09 +0100

    SPINNER

--- src/components/Books.svelte
+++ src/components/Books.svelte
@@ -3,6 +3,8 @@
 
   let works = [];
   let loading = false;
+  let firstLoad = true;
+  let loadingText = "Chill, your books are loading...";
   let offset = 0;
   let pageSize = 10;
 
@@ -14,10 +16,14 @@
     }
   };
 
-  const fetchData = async (firstLoad = false) => {
+  const fetchData = async () => {
     loading = true;
     const offsetUsed = firstLoad ? 0 : offset++;
     const pageSizeUsed = firstLoad ? 10 : pageSize + 10;
+
+    if (!firstLoad) {
+      loadingText = "Wow, you are a book worm. Scriptures incoming...";
+    }
 
     try {
       // const response = await fetch("https://randomuser.me/api/");
@@ -27,6 +33,7 @@
       const data = await response.json();
       works = data.works;
       loading = false;
+      firstLoad = false;
     } catch (error) {
       loading = false;
       console.error("Error fetching user:", error);
@@ -45,12 +52,21 @@
 </script>
 
 <div>
-  {#each works as workItem}
-    <div class="book-title">
-      <h1>{workItem.title} = {workItem.authors[0].name}</h1>
-      <p>{workItem.first_publish_year}</p>
+  {#if loading}
+    <div class="splash" class:further-load={!firstLoad}>
+      <div>
+        <span class="loader"></span>
+        <h3>{loadingText}</h3>
+      </div>
     </div>
-  {/each}
+  {:else}
+    {#each works as workItem}
+      <div class="book-title">
+        <h1>{workItem.title} = {workItem.authors[0].name}</h1>
+        <p>{workItem.first_publish_year}</p>
+      </div>
+    {/each}
+  {/if}
 </div>
 
 <style>
@@ -63,5 +79,83 @@
     border-radius: 20px;
     padding: 20px;
     margin: 10px;
+  }
+
+  .loader {
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    display: inline-block;
+    position: relative;
+    border: 3px solid;
+    border-color: #fff #fff transparent transparent;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite;
+  }
+  .loader::after,
+  .loader::before {
+    content: "";
+    box-sizing: border-box;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    border: 3px solid;
+    border-color: transparent transparent #ff3d00 #ff3d00;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    box-sizing: border-box;
+    animation: rotationBack 0.5s linear infinite;
+    transform-origin: center center;
+  }
+  .loader::before {
+    width: 32px;
+    height: 32px;
+    border-color: #fff #fff transparent transparent;
+    animation: rotation 1.5s linear infinite;
+  }
+
+  @keyframes rotation {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes rotationBack {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(-360deg);
+    }
+  }
+
+  .splash {
+    height: 100vh;
+    width: 100vw;
+    top: 0;
+    left: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .splash > div {
+    text-align: center;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .further-load {
+    top: initial;
+    height: 25vh;
   }
 </style>
